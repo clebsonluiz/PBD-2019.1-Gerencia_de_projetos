e2a81aca07425ceb249dc5efb2caa94704a61546
clebsonluiz <clebsonluiz5@gmail.com>
2019-04-05 01:16:30 -0300

    Commit 12 - Adicionado novas conversões de datas

--- br.com.pbd2019_1_ClébsonLuiz/br/com/pbd2019_1/utils/DateUtil.java
+++ br.com.pbd2019_1_ClébsonLuiz/br/com/pbd2019_1/utils/DateUtil.java
@@ -3,9 +3,34 @@ package br.com.pbd2019_1.utils;
 import java.sql.Date;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.ZoneId;
 
 public class DateUtil {
 
+	public static LocalDateTime getDateTime(LocalDate localDate, LocalTime localTime) {
+		return LocalDateTime.of(localDate, localTime);
+	}
+	
+	public static java.util.Date parceToDate(LocalDateTime localDateTime){
+		return Date.from(localDateTime.atZone(ZoneId.systemDefault()).toInstant());
+	}
+	
+	public static LocalDateTime parceToLocalDateTime(java.util.Date date){
+		return date.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
+	}
+	
+	public static LocalDate parseToLocalDate(java.util.Date date) {
+		return date.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+	}
+	
+	public static java.util.Date parseToDate(LocalDate localDate){
+		return Date.from(localDate.atStartOfDay(ZoneId.systemDefault()).toInstant());
+	}
+	
+	
 	public static Date getDataAtual(){
 		return getDateSQL(new java.util.Date());
 	}
